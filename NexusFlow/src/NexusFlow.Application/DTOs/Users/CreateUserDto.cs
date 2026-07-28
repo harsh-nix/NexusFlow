8501ef77b908e607f4bb371efa10df08a495eedd
Harsh Mishra <harshmishra85@yahoo.com>
2026-07-28 20:21:08 +0530

    admin create a new use DTO and admin edit a existing user DTO

---
+++ NexusFlow/src/NexusFlow.Application/DTOs/Users/CreateUserDto.cs
@@ -0,0 +1,13 @@
+﻿using NexusFlow.Domain.Enums;
+
+namespace NexusFlow.Application.DTOs.Users
+{
+    public class CreateUserDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.TeamMember;
+        public string? PhoneNumber { get; set; }
+    }
+}
---
+++ NexusFlow/src/NexusFlow.Application/DTOs/Users/UpdateUserDto.cs
@@ -0,0 +1,11 @@
+﻿using NexusFlow.Domain.Enums;
+
+namespace NexusFlow.Application.DTOs.Users
+{
+	public class UpdateUserDto
+	{
+		public string FullName { get; set; } = string.Empty;
+		public UserRole Role { get; set; }
+		public string? PhoneNumber { get; set; }
+	}
+}
